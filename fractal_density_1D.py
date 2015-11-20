2e8510f3f61ea1ad11f7f18259674983fc61083e
Duncan Forgan <dhf3@st-andrews.ac.uk>
2015-11-20 15:14:59 +0000

    Added a test to both 1D and 3D scripts Takes PDF of density, and fits against a lognormal
    Tidied up I/O significantly, and set up proper seeding for random
    number generation

--- fractal_density_1D.py
+++ fractal_density_1D.py
@@ -1,27 +1,43 @@
 # Written 16/11/15 by Duncan Forgan
-# Given an input power spectrum, generates fractal density grids in 3D
+# Given an input power spectrum, generates fractal density grids in 1D
+# Taken from Walch et al, arXiv:1109.3478
 
 import numpy as np
-from scipy.fftpack import fft, ifft, fftshift
+from scipy.fftpack import ifft
+from scipy.stats import norm
 import matplotlib.pyplot as plt
+from matplotlib.mlab import normpdf
+from datetime import datetime
 
-#fractalD = input("What is the fractal dimension? ")
-fractalD = 2.4
+# Read in input data:
+
+# fractalD - fractal dimension
+# rhozero - density scaling parameter
+# ngrid - Number of grid cells per dimension (i.e. total ngrid^3)
+# boxlength - Half length of box in code units (e.g. dimensions (-L,L))
+
+fractalD = input("What is the fractal dimension? ")
 
 npower = 2.0*(3.0-fractalD)+2
 
-print "This corresponds to a power spectrum with index ",npower
+print "Fractal Dimension is ", fractalD
+print "This corresponds to a power law power spectrum in k-space with index ",npower
 
-#rhozero = input("What is rhozero?")
-rhozero = 10.0
+rhozero = input("What is rhozero?")
 
 # Grid size and dimensions
 
-#ngrid = input("How many grid cells are required?")
-#boxlength = input("What is the box length (in pc)? ")
+ngrid = input("How many grid cells are required?")
+boxlength = input("What is the box length (in pc)? ")
 
-ngrid = 256
-boxlength = np.pi
+try:
+    seed = input("Input a random number seed, or press enter to generate it from system clock: ")
+except SyntaxError:
+    date = datetime.today()
+    print "Generating Random Number seed from current timestamp ", date
+    seed = np.int(date.hour + date.minute + date.second + date.day + date.month + date.year)
+    print "Seed is ", seed
+    
 
 dr = 2.0*boxlength/float(ngrid)
 ncells = ngrid*ngrid*ngrid
@@ -29,16 +45,22 @@ j = np.complex(0,1.0) # imaginary number
 
 # Output filename
 
-#filename = raw_input("What is the output filename? ")
-filename = 'trial'
+filename = raw_input("What is the output filename? ")
 
-# Construct wavenumbers
 
-k = np.zeros(ngrid) # kx, ky, kz identical for a given cell
+# Define the Grid
+print "Defining Grid"
+print "Boxlength: ", boxlength
+print "Cell Size: ", dr
+
+k = np.zeros(ngrid)
 x = np.zeros(ngrid)
 
 for ix in range(ngrid):
     x[ix] = - boxlength + ix*dr
+
+print "Constructing Wavenumbers"
+
     
 # Construct k such that it can be fed into ifft    
 # k[0] = zero frequency
@@ -49,15 +71,17 @@ k[0] = 0.0
 
 # positive frequencies first
 for ix in range(1,ngrid/2+1):
-    #k[ix] = ix
     k[ix] = 2.0*np.pi/(2.0*boxlength-ix*dr)
-    #print k[ix], boxlength-ix*dr
+    
     
 # Now negative frequencies
 for ix in range(1,ngrid/2):
     k[ngrid/2+ix] = -k[ngrid/2-ix]
     
 # Construct Power Spectrum
+
+print '--'
+print "Computing Power Spectrum"
 
 powerspec = np.zeros(ngrid)
 
@@ -67,11 +91,14 @@ for ix in range(ngrid):
     if(kmag==0.0): powerspec[ix]=0.0
     else: powerspec[ix] = np.power(kmag,-npower)
 
-        
+# The power spectrum has no phase data - must generate it before Fourier Transform
 # Use random phases to reconstruct the Fourier Transform of the density
-# Need to be careful about use of complex numbers here
 
 rho_fft = np.sqrt(powerspec)
+
+randgen = np.random.RandomState(seed)
+
+print "Computing Random Phases, seed: ",seed
 
 phases = j*2.0*np.pi*np.random.random(ngrid)
 phases = np.exp(phases)
@@ -80,44 +107,66 @@ rho_fft = rho_fft*phases
             
 # Compute the inverse fourier transform of rho_fft
 
+print "--"
+print "Carrying out Inverse FFT"
+
 rho = ifft(rho_fft)
 
-print "x: ", x
-print "rho: ", rho
-# Scale density by rhozero
-
+# Scale by rhozero
+print "Scaling Density"
 rho = np.real(np.exp(rho/rhozero))
 
-print "rho scaled: ", rho
-
-fig1 = plt.figure()
-ax1 = fig1.add_subplot(111)
-ax1.scatter(fftshift(k), fftshift(powerspec))
-ax1.scatter(k, fft(rho_fft), color = 'green')
-#ax1.plot(x,rho)
+meanrho = np.mean(rho.flatten())
+rhotest = np.log10(rho.flatten()/meanrho)
 
 fig2 = plt.figure()
 ax2 = fig2.add_subplot(111)
-ax2.plot(x, rho)
+ax2.plot(x, rhotest)
+
+(mu,sigma) = norm.fit(rhotest)
+
+# Test that the density histogram is lognormal
+
+fig3 = plt.figure()
+ax3 = fig3.add_subplot(111)
+n, bins, patches = ax3.hist(rhotest, bins = ngrid)
+
+histmax = np.amax(n)
+yfit = normpdf(bins, mu,sigma)
+
+ymax = np.amax(yfit)
+
+# Scale the normal distribution to fit
+yfit = histmax*yfit/ymax
+
+ax3.plot(bins, yfit)
+ax3.set_xlabel(r'log $\frac{\rho}{\bar{\rho}}$', fontsize = 16)
+ax3.set_ylabel(r'PDF', fontsize = 16)
 plt.show()
 
+print "Density PDF is fitted by a lognormal with parameters: "
+print "Mean: ",mu
+print "Sigma: ", sigma
+
 # Write density grid to file
+print "--"
+print "Writing to file ", filename
 
 f_obj = open(filename, 'w')
 
-# First write number of x, y and z cells to header
+# First write number of x cells to header
 
-line = str(ngrid)+' '+str(ngrid)+ ' ' +str(ngrid) 
+line = str(ngrid)
 
 f_obj.write(line+'\n')
 
-
-
 # Now write in format
-# xcell rho  (x,y,z co-ordinates being leftward cell face)
+# xcell rho  (x co-ordinates being leftward cell face)
 
 for ix in range(ngrid):
     line = str(x[ix]) +' '+ str(rho[ix])
     f_obj.write(line+'\n')
 
-f_obj.close()        
+f_obj.close()    
+
+print "File Write Complete"      
--- fractal_density_3D.py
+++ fractal_density_3D.py
@@ -1,30 +1,43 @@
 # Written 16/11/15 by Duncan Forgan
 # Given an input power spectrum, generates fractal density grids in 3D
+# Taken from Walch et al, arXiv:1109.3478
 
 import numpy as np
 from scipy.fftpack import ifftn
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+from matplotlib.mlab import normpdf
+from datetime import datetime
 
-# Read in fractal dimension
+# Read in input data:
 
+# fractalD - fractal dimension
+# rhozero - density scaling parameter
+# ngrid - Number of grid cells per dimension (i.e. total ngrid^3)
+# boxlength - Half length of box in code units (e.g. dimensions (-L,L))
 
-#fractalD = input("What is the fractal dimension? ")
-fractalD = 2.4
+fractalD = input("What is the fractal dimension? ")
 
 npower = 2.0*(3.0-fractalD)+2
 
 print "Fractal Dimension is ", fractalD
 print "This corresponds to a power law power spectrum in k-space with index ",npower
 
-#rhozero = input("What is rhozero?")
-rhozero = 1.0
+rhozero = input("What is rhozero?")
 
 # Grid size and dimensions
 
-#ngrid = input("How many grid cells are required?")
-#boxlength = input("What is the box length (in pc)? ")
+ngrid = input("How many grid cells are required?")
+boxlength = input("What is the box length (in pc)? ")
 
-ngrid = 16
-boxlength = 1.0
+try:
+    seed = input("Input a random number seed, or press enter to generate it from system clock: ")
+except SyntaxError:
+    date = datetime.today()
+    print "Generating Random Number seed from current timestamp ", date
+    seed = np.int(date.hour + date.minute + date.second + date.day + date.month + date.year)
+    print "Seed is ", seed
+    
 
 dr = 2.0*boxlength/float(ngrid)
 ncells = ngrid*ngrid*ngrid
@@ -32,8 +45,7 @@ j = np.complex(0,1.0) # imaginary number
 
 # Output filename
 
-#filename = raw_input("What is the output filename? ")
-filename = 'trial'
+filename = raw_input("What is the output filename? ")
 
 # Construct wavenumbers
 
@@ -51,7 +63,7 @@ for ix in range(ngrid):
     y[ix] = x[ix]
     z[ix] = x[ix]
     
-# Construct k such that it can be fed into ifft    
+# Construct k such that it can be correctly fed into ifftn   
 # k[0] = zero frequency
 # k[1:ngrid/2+1] = positive frequencies (ascending)
 # k[ngrid/2+1:-1] = negative frequencies (decreasingly negative)
@@ -79,17 +91,19 @@ powerspec = np.zeros((ngrid,ngrid,ngrid))
 for ix in range(1,ngrid):
     for iy in range(1,ngrid):
         for iz in range(1,ngrid):
-            kmag= np.sqrt(k[ix]*k[ix]+ k[iy]*k[iy] + k[iz]*k[iz])
             
+            kmag= np.sqrt(k[ix]*k[ix]+ k[iy]*k[iy] + k[iz]*k[iz])
             powerspec[ix,iy,iz] = np.power(kmag,-npower)
             
 
+# The power spectrum has no phase data - must generate it before Fourier Transform
 # Use random phases to reconstruct the Fourier Transform of the density
-# Need to be careful about use of complex numbers here
 
 rho_fft = np.sqrt(powerspec)
 
-print "Computing Random Phases"
+randgen = np.random.RandomState(seed)
+
+print "Computing Random Phases, seed: ",seed
 phases = j*2.0*np.pi*np.random.random((ngrid,ngrid,ngrid))
 phases = np.exp(phases)
 
@@ -106,6 +120,33 @@ rho = ifftn(rho_fft)
 
 print "Scaling Density"
 rho = np.real(np.exp(rho/rhozero))
+
+meanrho = np.mean(rho.flatten())
+rhotest = np.log10(rho.flatten()/meanrho)
+
+(mu,sigma) = norm.fit(rhotest)
+
+# Test that the density histogram is lognormal
+
+fig3 = plt.figure()
+ax3 = fig3.add_subplot(111)
+n, bins, patches = ax3.hist(rhotest, bins = ngrid)
+
+histmax = np.amax(n)
+yfit = normpdf(bins, mu,sigma)
+
+ymax = np.amax(yfit)
+# Scale the normal distribution to fit
+yfit = histmax*yfit/ymax
+
+ax3.plot(bins, yfit)
+ax3.set_xlabel(r'log $\frac{\rho}{\bar{\rho}}$', fontsize = 16)
+ax3.set_ylabel(r'PDF', fontsize = 16)
+plt.show()
+
+print "Density PDF is fitted by a lognormal with parameters: "
+print "Mean: ",mu
+print "Sigma: ", sigma
 
 # Write density grid to file
 
